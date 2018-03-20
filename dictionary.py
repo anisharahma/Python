035846d3e4347ebdc7574854cccbcdce1a337727
anisharahma <37042430+anisharahma@users.noreply.github.com>
2018-03-20 03:51:50 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,3 @@
-Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> #masukkan data nomor telpon
 >>> no_telpon = { "nama" : ["Jane doe", "John Smith", "Bob Stone"], "Telephone Number" : ["+27 555 5367", "+27 555 6254", "+27 555 5689"] }
 >>> #ubah nomor telpon Jane dengan nomor +27 555 1024
@@ -7,7 +5,7 @@ Type "copyright", "credits" or "license()" for more information.
 >>> no_telpon
 {'nama': ['Jane doe', 'John Smith', 'Bob Stone'], 'Telephone Number': ['+27 555 1024', '+27 555 6254', '+27 555 5689']}
 >>> #tambahkan data baru dengan nama Anna Cooper dengan no telpon +27 555 3237
->>> no_telpon.update({ "nama" : ["Jane doe", "John Smith", "Bob Stone", "Anna Cooper"], "Telephone Number" : ["+27 555 5367", "+27 555 6254", "+27 555 5689", "+27 555 3237"] })
+>>> no_telpon.update({ "nama" : ["Jane doe", "John Smith", "Bob Stone", "Anna Cooper"], "Telephone Number" : ["+27 555 1024", "+27 555 6254", "+27 555 5689", "+27 555 3237"] })
 >>> no_telpon
 {'nama': ['Jane doe', 'John Smith', 'Bob Stone', 'Anna Cooper'], 'Telephone Number': ['+27 555 5367', '+27 555 6254', '+27 555 5689', '+27 555 3237']}
 >>> #cetak nomor telpon Bob
