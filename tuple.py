eb166c9a0c74147d80695a595197a91e9e14717d
anisharahma <37042430+anisharahma@users.noreply.github.com>
2018-03-20 03:47:40 +0000

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,23 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #buat tuple a yang berisi 4 bilangan positif pertama dan tuple b yang berisi 4 bilangan bulat positif berikutnya
+>>> a = (1, 2, 3, 4)
+>>> b = (5, 6, 7, 8)
+>>> #buat tuple c yang menggabungkan semua angka a dan b
+>>> c = a+b;
+>>> print c
+(1, 2, 3, 4, 5, 6, 7, 8)
+>>> #buat tuple d yang merupakan salinan dari c
+>>> d = c;
+>>> print d
+(1, 2, 3, 4, 5, 6, 7, 8)
+>>> #cetak elemen ketiga d
+>>> print d[2]
+3
+>>> #cetak 3 elemen terakhir dari d tanpa menggunakan panjangnya
+>>> print d[-3:8]
+(6, 7, 8)
+>>> #cetak panjang d
+>>> print len(d)
+8
+>>> 
