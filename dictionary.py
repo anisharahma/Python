d81cf6dc8293c8079b80b44ec34614ed3dbcd799
anisharahma <37042430+anisharahma@users.noreply.github.com>
2018-03-27 02:57:35 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,7 @@
 >>> #tambahkan data baru dengan nama Anna Cooper dengan no telpon +27 555 3237
 >>> no_telpon.update({ "nama" : ["Jane doe", "John Smith", "Bob Stone", "Anna Cooper"], "Telephone Number" : ["+27 555 1024", "+27 555 6254", "+27 555 5689", "+27 555 3237"] })
 >>> no_telpon
-{'nama': ['Jane doe', 'John Smith', 'Bob Stone', 'Anna Cooper'], 'Telephone Number': ['+27 555 5367', '+27 555 6254', '+27 555 5689', '+27 555 3237']}
+{'nama': ['Jane doe', 'John Smith', 'Bob Stone', 'Anna Cooper'], 'Telephone Number': ['+27 555 1024', '+27 555 6254', '+27 555 5689', '+27 555 3237']}
 >>> #cetak nomor telpon Bob
 >>> no_telpon["Telephone Number"][2]
 '+27 555 5689'
